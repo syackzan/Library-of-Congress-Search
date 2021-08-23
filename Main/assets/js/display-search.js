917ce8bf7d86d69afd524b2c8666e4c373eda0cb
Scotty Yackzan <syackzan@gmail.com>
2021-08-23 11:32:06 -0700

    Moved Starter Code into Repository

---
+++ Main/assets/js/display-search.js
@@ -0,0 +1,16 @@
+var resultTextEl = document.querySelector('#result-text');
+var resultContentEl = document.querySelector('#result-content');
+var searchFormEl = document.querySelector('#search-form');
+
+function getParams() {
+  // Get the search params out of the URL (i.e. `?q=london&format=photo`) and convert it to an array (i.e. ['?q=london', 'format=photo'])
+  var searchParamsArr = document.location.search.split('&');
+
+  // Get the query and format values
+  var query = searchParamsArr[0].split('=').pop();
+  var format = searchParamsArr[1].split('=').pop();
+
+  searchApi(query, format);
+}
+
+getParams()
