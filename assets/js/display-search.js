3e37f4a980cef104775da060877610cf8c020cf8
Scotty Yackzan <syackzan@gmail.com>
2021-08-23 13:40:31 -0700

    Updated request Url to search

--- assets/js/display-search.js
+++ assets/js/display-search.js
@@ -5,11 +5,12 @@ var searchFormEl = document.querySelector('#search-form');
 function getParams() {
   // Get the search params out of the URL (i.e. `?q=london&format=photo`) and convert it to an array (i.e. ['?q=london', 'format=photo'])
   var searchParamsArr = document.location.search.split('&');
-
+  console.log(searchParamsArr);
   // Get the query and format values
   var query = searchParamsArr[0].split('=').pop();
-  var queryR = query.replace("%20", "+");
   var format = searchParamsArr[1].split('=').pop();
+  format = format.replace("=", "");
+  var queryR = query.replace("%20", "+");
   console.log(query);
   console.log(queryR);
   console.log(format);
@@ -18,7 +19,7 @@ function getParams() {
 }
 
 function searchApi(query, format){
-  var requestUrl = "https://www.loc.gov/" + format + "/?q=" + query; 
+  var requestUrl = "https://www.loc.gov/" + format + "/?q=" + query + "&fo=json"; 
   console.log(requestUrl);
   
   fetch(requestUrl)
